ca140c743aafa53b7382d8dfb7000ab0f37d3304
Amir Marvasti Nejad <amir.m.n.1380@gmail.com>
2020-06-15 21:47:32 +0430

    Uni

--- Uni/uni/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student,Admin,Exter,Ostad,Elam,Klass
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Admin)
-admin.site.register(Exter)
-admin.site.register(Ostad)
-admin.site.register(Elam)
-admin.site.register(Klass)
